1749bca1b8f2269f7d095450783e129283f0e0ed
Timur Mukhametyanov <canfus60@gmail.com>
2023-12-22 23:39:40 +0300

    fix: fix volume icon

--- src/common/widget/mediaPlayer/volume/volume.tsx
+++ src/common/widget/mediaPlayer/volume/volume.tsx
@@ -1,15 +1,7 @@
-/* eslint-disable no-nested-ternary,operator-linebreak */
 import { FC } from 'react';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { Root, Trigger, Portal, Content, Arrow } from '@radix-ui/react-tooltip';
 
-import {
-  VolumeMinIcon,
-  VolumeMaxIcon,
-  VolumeOffIcon,
-  Slider,
-  IconButton,
-} from '@app/common';
+import { VolumeMaxIcon, VolumeOffIcon, Slider, IconButton } from '@app/common';
 
 import type { VolumeProps } from './volume.interface';
 import styles from './volume.module.css';
@@ -23,15 +15,7 @@ export const Volume: FC<VolumeProps> = ({
   <Root delayDuration={100} {...props}>
     <Trigger asChild>
       <IconButton
-        icon={
-          volume[0] > 0 ? (
-            <VolumeMinIcon />
-          ) : volume[0] > 75 ? (
-            <VolumeMaxIcon />
-          ) : (
-            <VolumeOffIcon />
-          )
-        }
+        icon={volume[0] > 0 ? <VolumeMaxIcon /> : <VolumeOffIcon />}
       />
     </Trigger>
     <Portal>
