2e7590a627c52b86e0995f6ef0f62182319fc6c4
Canfus <canfus69@gmail.com>
2024-01-01 02:18:18 +0300

    fix: add auth condition

--- src/pages/public/auth/layout.page.tsx
+++ src/pages/public/auth/layout.page.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useMatch, Navigate } from 'react-router-dom';
 
 import { Logo, LogoMobile, useUserAgent } from '@app/common';
 
@@ -6,6 +6,17 @@ import styles from './layout.module.css';
 
 export const AuthLayout = () => {
   const [isMobile] = useUserAgent({ breakpoint: 768 });
+
+  // this condition will return, we inside only auth route or inside of children routes
+  // for example: http://localhost:3000/auth - this we inside only auth route
+  // for example: http://localhost:3000/auth/login - this we inside of children routes
+  const isInsideAuthPage = Boolean(useMatch('/auth/*')?.params['*']);
+
+  // if we inside only auth route, navigate to login page by default
+  // overwise skip this condition
+  if (!isInsideAuthPage) {
+    return <Navigate to="/auth/login" />;
+  }
 
   return (
     <div className={styles.layout}>
